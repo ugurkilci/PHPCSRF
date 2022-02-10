fa45b3d64d7dca9fe258ad68de2f31849dedb213
Uğur KILCI <1714709016@ogr.mehmetakif.edu.tr>
2022-02-10 08:57:53 +0300

    Create csrf.php

---
+++ csrf.php
@@ -0,0 +1,28 @@
+<?php
+
+$_SESSION["_token"] = bin2hex(random_bytes(32)); // Random bytes + safe cryptography
+
+// CSRF
+$token = filter_input(INPUT_POST, '_token', FILTER_SANITIZE_STRING); // Only input datas
+
+if (!$token || ($token !== $_SESSION['_token'])) {
+  header($_SERVER['SERVER_PROTOCOL'] . ' 405 Method Not Allowed'); // go to ERROR!
+  exit;
+}
+
+
+###############################
+
+// Example:
+
+if ($_POST) 
+{
+  
+  $token = filter_input(INPUT_POST, '_token', FILTER_SANITIZE_STRING);
+  if (!$token || ($token !== $_SESSION['_token'])) {
+    header($_SERVER['SERVER_PROTOCOL'] . ' 405 Method Not Allowed');
+    exit;
+  }
+  
+  // POST Operations
+}
